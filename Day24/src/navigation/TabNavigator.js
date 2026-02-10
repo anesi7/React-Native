63d2510ebd6c52596659ff5b4266509277dbe73a
anesi7 <anesvitija7@gmail.com>
2026-02-10 20:40:38 +0100

    Day24

---
+++ Day24/src/navigation/TabNavigator.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import { MainStackNavigator, AboutStackNavigator } from "./StackNavigator";
+import IosStackNavigator from "./IosStackNavigator";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#00BCD4",
+        tabBarInactiveTintColor: "lightgray",
+        tabBarShowLabel: true,
+        tabBarStyle: {
+          backgroundColor: "#2C3E50",
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Ios"
+        component={IosStackNavigator}
+        options={{
+          tabBarLabel: "android",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="cellphone-iphone"
+              size={26}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={AndroidStackNavigator}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home"
+              color={color}
+              size={size ?? 26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={IosStackNavigator}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="home"
+              color={color}
+              size={size ?? 26}
+            />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="About"
+        component={AboutStackNavigator}
+        options={{
+          tabBarLabel: "About",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="video-stabilization"
+              color={color}
+              size={size ?? 26}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTabNavigator;
